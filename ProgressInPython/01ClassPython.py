808f2a7d666ba7f2a1f29ef600384857e76c9493
zanrock <luisenin@hotmail.com>
2024-10-06 01:01:00 -0500

    fourcommit

---
+++ ProgressInPython/01ClassPython.py
@@ -0,0 +1,45 @@
+"""En esta clase veremos lo siguiente """
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+"""esto es lo que pude descargar del repositorio de 30days de python a qui abajo are un par de ejercicios simples para 
+tener una buena retroalimentacion de lo que se esta haciendo are varios ejemplos con los tipos de datos que tenemos en python"""
+
+# ejemplo de addition (+)
+
+num1 = "manzanas" 
+num2 = "peras"
+num3 = 'cebolla' 
+
+print("A Luis le gustan las", num1 ,"odia las", num3 ,"y siempre se le roba las ",num2, "de su hermana")
+
+# ejemplo de sustraccion (-)
+
+n1 = 10
+n2 = 20
+n3 = 30
+
+print(n1 + n2 - n3 + n2)
+
+# ejemplo  multiplication(*)
+
+print(n1 * n3 - n1)
--- ProgressInPython/main.py
+++ ProgressInPython/main.py
@@ -1,3 +1,3 @@
 print("hello world")
-name = luis
-print("mi nombre es" + name)
+name = "luis"
+print("mi nombre es" +  name)
