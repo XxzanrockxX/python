5865cd3e56d54c0b70991631914f1a1e92405681
zanrock <luisenin@hotmail.com>
2024-10-19 01:42:34 -0500

    funciones incorporadas python DF

--- ProgressInPython/02ClassPhyton.py
+++ ProgressInPython/02ClassPhyton.py
@@ -1,1 +1,22 @@
-"""en esta clase veremos lo que son variables"""
+"""Funciones integradas"""
+# estas son las funciones integradas mas usadas en Python
+print() # esta funcion lo que hace es que permite imprimir algo en pantalla 
+len()   # esta funcion cuenta el numero de caracteres incluyendo el espacio
+type()  # esta funcion muestra el tipo de dato 
+int()   # esta funcion convierte un string en un numero 
+float() # esta funcion comvierte un entero a decimal 
+str()   # esta funcion convierte un numero a string
+input() # esta funcion pausa la ejecucion del programa muestra un mensaje opcional al usuario y devuelve la cadena de texto ingresada por el usuario como entrada estandar.
+list()  # esta funcion se encarga de creaar nuevos objetos tipo listas 
+dict()  # esta funcion crea diccionarios, estos son estructuras de datos que almacenan pares clave-valor clave debe ser unica y se utiliza para acceder al valor asociado.
+min()   # esta funcion se utiliza para encontrar el elemento mas pequeño dentro de un iterable estos pueden ser (listas,tupla,conjunto,etc.) o entre varios argumentos.
+max()   # esta funcion es la inversa de min ya que esta busca el valor mayor.
+sum()   # esta funcion se usa para sumar todos los elementos numericos dentro de un iterable.
+sorted()# esta funcion crea una lista ordenada apartir de un iterable, sin modificar el iterable original.
+open()  # esta funcion se utiliza para abrir archivos 
+file()  # un dato curioso con esta funcion y es que para python3 ya no existe y ha sido remplazada por la funcion open() asi que ahora se usa open() para crear obetos de archivo en el sistea de archivos 
+help()  # esta funcion se utiliza para obtener iformacion de cualquier elemento del lenguaje
+dir()   #esta funcion devuelve una lista de cadenas que contienen los nombres de los atributos y metodos de un objeto si se llaman sin argumentos , dir() devolvera los nombres que se encuentran actualmente en el ambito local.
+
+"""con esto damos una descripcion de lo que hacen estas funciones incorporadas"""
+
